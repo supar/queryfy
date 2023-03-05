f77fc5cec15d1c31bef1747b87aa3b4969a77505
supar <paulrez@gmail.com>
2023-03-05 17:33:00 +0400

    Combine all in main()

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
-	"sync"
 	"time"
 )
 
@@ -17,165 +18,35 @@ var (
 	ErrUprocessableEntity = errors.New("unprocessable entity")
 )
 
+var (
+	addrStr  = flag.String("a", ":8080", "address:port to listen")
+	limitStr = flag.Uint("l", 100, "limit incoming requests")
+)
+
 func main() {
+	flag.Parse()
 
-}
+	lmr := newLimiter(uint8(*limitStr))
 
-type Disclosure struct {
-	// http Client (in case any transport optimization)
-	cc *http.Client
-
-	// any error raised in progress
-	er error
-
-	// response
-	resp ResponseData
-	// url list to request
-	urls []string
-
-	// progress is on, will be closed on error
-	rn chan struct{}
-	// active queury buffer
-	qw chan struct{}
-
-	// once stop function to close rn chan
-	stop func()
-}
-
-func NewDisclosure(data []string, qln int) *Disclosure {
-	if qln <= 0 {
-		qln = 4
+	srv := &http.Server{
+		Addr:    *addrStr,
+		Handler: lmr.middleware(http.HandlerFunc(handler)),
 	}
 
-	ds := &Disclosure{
-		cc: http.DefaultClient,
+	wait := newShutdownSignal(func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
 
-		resp: make(ResponseData, len(data)),
-		urls: data,
-
-		rn: make(chan struct{}),
-		qw: make(chan struct{}, qln),
-	}
-
-	var once sync.Once
-	ds.stop = func() {
-		once.Do(func() {
-			close(ds.rn)
-		})
-	}
-
-	return ds
-}
-
-func (d *Disclosure) do(pctx context.Context, idx int) {
-	req, err := http.NewRequest("GET", d.urls[idx], nil)
-	if err != nil {
-		err = fmt.Errorf("%s: %w", err.Error(), ErrBadURL)
-		d.stopWithError(err)
-		return
-	}
-
-	dn := make(chan error, 1)
-
-	ctx, cancel := context.WithTimeout(pctx, 1*time.Second)
-	defer cancel()
-
-	req = req.WithContext(ctx)
-
-	go func() {
-		dn <- func(r *http.Response, err error) error {
-			if err != nil {
-				return err
-			}
-			defer r.Body.Close()
-
-			if r.StatusCode >= 400 {
-				err = fmt.Errorf("remote server code = %d", r.StatusCode)
-				return err
-			}
-
-			var body []byte
-			body, err = io.ReadAll(r.Body)
-			if err != nil {
-				return err
-			}
-
-			d.resp[idx] = URLResult{
-				URL:    d.urls[idx],
-				Result: string(body),
-			}
-
-			return nil
-		}(d.cc.Do(req))
-	}()
-
-	select {
-	case <-d.rn:
-		return
-
-	case <-ctx.Done():
-		<-dn
-		err = ctx.Err()
-
-	case err = <-dn:
-		if err != nil {
-			err = fmt.Errorf("url item: %s: %w", err.Error(), ErrUprocessableEntity)
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("Shutdown: %v", err)
 		}
+	})
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("Start: %v", err)
 	}
 
-	if err != nil {
-		d.stopWithError(err)
-	}
-}
-
-func (d *Disclosure) stopWithError(err error) {
-	if d.er == nil {
-		d.er = err
-	}
-	d.stop()
-}
-
-func (d *Disclosure) Walk(pctx context.Context) (err error) {
-	ctx, cancel := context.WithCancel(pctx)
-	defer cancel()
-
-	wg := sync.WaitGroup{}
-
-	var i int
-
-walk:
-	for off := len(d.urls); i < off; i++ {
-		select {
-		case <-d.rn:
-			cancel()
-			break walk
-
-		case d.qw <- struct{}{}:
-			wg.Add(1)
-
-			go func(idx int) {
-				defer func() {
-					<-d.qw
-					wg.Done()
-				}()
-
-				d.do(ctx, idx)
-			}(i)
-		}
-	}
-
-	wg.Wait()
-
-	return nil
-}
-
-// ResponseData represents server response for the requested URLs list
-type ResponseData []URLResult
-
-// URLResult represetns response item in the ResponseData
-type URLResult struct {
-	URL    string `json:"url"`
-	Result string `json:"result"`
+	<-wait
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -190,7 +61,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ds := NewDisclosure(data, 4)
+	ds := NewCollection(data, 4)
 	ds.Walk(r.Context())
 
 	if ds.er != nil {
